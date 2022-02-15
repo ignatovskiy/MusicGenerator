936e1e2072e935b4906969983d132a437d4b5bbc
ignatovskiy <naignatovskiy@edu.hse.ru>
2022-02-15 23:42:48 +0300

    Add background for index.html.

--- web/server.py
+++ web/server.py
@@ -1,10 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="template")
 
 
 @app.route('/')
 def render_start_page():
-    return 'Test.'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
