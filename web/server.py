862f91ce88007c9c30249df4b12fb5e4773f94cf
ignatovskiy <naignatovskiy@edu.hse.ru>
2022-02-17 19:08:31 +0300

    Add basic navbar for site.

--- web/server.py
+++ web/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder="template", static_folder='static')
 
 @app.route('/')
 def render_start_page():
-    return render_template("index.html")
+    return render_template("index.html", indicator_value=5)
 
 
 if __name__ == '__main__':
