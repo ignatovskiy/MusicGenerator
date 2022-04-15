b3a80afc440025563af14154c66ee66ca6fc33f1
ignatovskiy <naignatovskiy@edu.hse.ru>
2022-04-15 22:35:56 +0300

    Added weights saving to file.

--- neuro/model.py
+++ neuro/model.py
@@ -6,3 +6,12 @@ MODELS_FOLDER = './model'
 
 def load_weights_from_file(temp_model, epochs=100):
     temp_model.load_weights(os.path.join(MODELS_FOLDER, 'weights.{}.h5'.format(epochs)))
+
+
+def save_weights_to_file(temp_model, epochs=100):
+    if not os.path.exists(MODELS_FOLDER):
+        os.makedirs(MODELS_FOLDER)
+
+    temp_model.save_weights(os.path.join(MODELS_FOLDER, "weights.{}.h5".format(epochs)))
+
+
