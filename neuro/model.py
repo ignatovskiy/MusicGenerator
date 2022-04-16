164571d26b59b734aa243aaef266a0b2f1610070
ignatovskiy <naignatovskiy@edu.hse.ru>
2022-04-16 03:22:26 +0300

    Added model creating func.

--- neuro/model.py
+++ neuro/model.py
@@ -1,5 +1,8 @@
 import os
 
+from keras.layers import Activation, Dense, Dropout, Embedding, LSTM, TimeDistributed
+from keras.models import load_model, Sequential
+
 
 MODELS_FOLDER = './model'
 
@@ -14,4 +17,18 @@ def save_weights_to_file(temp_model, epochs=100):
 
     temp_model.save_weights(os.path.join(MODELS_FOLDER, "weights.{}.h5".format(epochs)))
 
+
+def create_model(batch_size, sequence_len, vocabulary_size):
+    temp_model = Sequential()
+
+    temp_model.add(Embedding(vocabulary_size, 512, batch_input_shape=(batch_size, sequence_len)))
+
+    for _ in range(3):
+        temp_model.add(LSTM(256, stateful=True, return_sequences=True))
+        temp_model.add(Dropout(0.2))
+
+    temp_model.add(TimeDistributed(Dense(vocabulary_size)))
+    temp_model.add(Activation('softmax'))
+
+    return temp_model
 
