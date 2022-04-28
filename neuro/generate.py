5f85f0fdcff40e1f4346c16e4ad6b04166ce7d5b
ignatovskiy <naignatovskiy@edu.hse.ru>
2022-04-28 22:14:41 +0300

    Add building models for generating script.

--- neuro/generate.py
+++ neuro/generate.py
@@ -1,10 +1,19 @@
-def build_sample_model(vocab_size):
-    model = Sequential()
-    model.add(Embedding(vocab_size, 512, batch_input_shape=(1, 1)))
-    for i in range(3):
-        model.add(LSTM(256, return_sequences=(i != 2), stateful=True))
-        model.add(Dropout(0.2))
+from keras.layers import Activation, Dense, Dropout, Embedding, LSTM
+from keras.models import Sequential
 
-    model.add(Dense(vocab_size))
-    model.add(Activation('softmax'))
-    return model
+
+def build_model(vocabulary_size):
+
+    final_model = Sequential()
+
+    final_model.add(Embedding(vocabulary_size, 512, batch_input_shape=(1, 1)))
+
+    for i in range(3):
+        final_model.add(LSTM(256, stateful=True, return_sequences=(i != 2)))
+        final_model.add(Dropout(0.2))
+
+    final_model.add(Dense(vocabulary_size))
+
+    final_model.add(Activation('softmax'))
+
+    return final_model
