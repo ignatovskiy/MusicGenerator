ff794992684c8a76db34b0bae8d2418d762cb3a1
ignatovskiy <naignatovskiy@edu.hse.ru>
2022-05-08 04:26:18 +0300

    Add argparse cmd tools for converter.py

--- neuro/converter.py
+++ neuro/converter.py
@@ -1,6 +1,20 @@
+import argparse
+
 from music21 import converter
 
 
 def main(in_path, out_path):
     abc = converter.parse(str(in_path))
     abc.write('midi', fp=str(out_path))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert .abc files to .mid')
+
+    parser.add_argument('--into', default='', help='input file (in abc notation)')
+    parser.add_argument('--out', default='', help='output file (midi format)')
+
+    args = parser.parse_args()
+    print(args.into, args.out)
+
+    main(args.into, args.out)
